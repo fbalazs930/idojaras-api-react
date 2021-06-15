abb3b055fd27dc626bf07b0898aedc3e4f25f7e5
fbalazs930 <fbalazs930@gmail.com>
2021-06-15 19:41:04 +0200

    Redesign

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     Axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2cbb02872cc7e2a1ee53454e4b98b7db&lang=hu&units=metric`).then((response) => {
       setCityName(city);
       setTemp(response.data.main.temp);
+      console.log(response.data);
       setDesc(response.data.weather[0].description);
       setHumidity(response.data.main.humidity);
       setWind(Math.round(response.data.wind.speed * 3.6, 0));
@@ -34,27 +35,38 @@ const App = () => {
           }
           }
         />
-        <button type="button" onClick={() => getData()}>Search</button>
+        <button type="button" onClick={getData}>Search</button>
       </div>
-      <div style={cityName.length > 0 ? { opacity: '1' } : { opacity: '0' }} className="weather">
-        <h1 className="name" id="name">
-          {cityName}
-        </h1>
-        <img src={`http://openweathermap.org/img/wn/${img}.png`} alt="" className="weather-image" />
-        <p className="temp">
-          {temp} &deg;C
-        </p>
+      <div style={cityName.length > 0 ? { opacity: '1' } : { opacity: '0' }} className="weather-infos">
+        <div className="left">
+          <img src={`http://openweathermap.org/img/wn/${img}.png`} alt="" className="image" />
+          <div className="units">
+            <p>{Math.round(temp, 2)}</p>
+            <p>&deg;C</p>
+            <p>&deg;F</p>
+          </div>
+      </div>
+      <div className="mid">
         <p className="humidity">
           Páratartalom: {humidity} %
         </p>
         <p className="wind">
           Szél: {wind} km/h
         </p>
+      </div>
+      <div className="right">
+        <h1 className="name" id="name">
+          {cityName}
+        </h1>
+        <div className="time">
+          time
+        </div>
         <p className="desc">
           {desc.charAt(0).toUpperCase() + desc.slice(1)}
         </p>
       </div>
     </div>
+    </div >
   );
 }
 
