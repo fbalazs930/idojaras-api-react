a8c7237dfc8ff24072373e9e38db27512a209a16
fbalazs930 <fbalazs930@gmail.com>
2021-06-15 18:52:05 +0200

    Add Icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Axios from 'axios';
 import './App.css';
 
@@ -7,9 +7,9 @@ const App = () => {
   const [cityName, setCityName] = useState("");
   const [temp, setTemp] = useState(0);
   const [desc, setDesc] = useState("");
-  const [weatherImg, setWeatherImg] = useState("");
   const [humidity, setHumidity] = useState(0);
   const [wind, setWind] = useState(0);
+  const [img, setImg] = useState('01d');
   const getData = () => {
     Axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2cbb02872cc7e2a1ee53454e4b98b7db&lang=hu&units=metric`).then((response) => {
       setCityName(city);
@@ -17,33 +17,9 @@ const App = () => {
       setDesc(response.data.weather[0].description);
       setHumidity(response.data.main.humidity);
       setWind(Math.round(response.data.wind.speed * 3.6, 0));
+      setImg(response.data.weather[0].icon);
     })
   }
-  useEffect(() => {
-    switch (desc) {
-      case "tiszta égbolt":
-        setWeatherImg("https://i.postimg.cc/XYWxrv6s/sunny.png");
-        break;
-      case "kevés felhő":
-        setWeatherImg("https://i.postimg.cc/Hkw2C8jp/cloudy.png");
-        break;
-      case "erős felhőzet":
-        setWeatherImg("https://i.postimg.cc/Hkw2C8jp/cloudy.png");
-        break;
-      case "borús égbolt":
-        setWeatherImg("https://i.postimg.cc/Hkw2C8jp/cloudy.png");
-        break;
-      case "köd":
-        setWeatherImg("https://i.postimg.cc/HWr9khjv/fog.png");
-        break;
-      case "enyhe eső":
-        setWeatherImg("https://i.postimg.cc/s2P5bk61/rain-s-cloudy.png");
-        break;
-      default:
-        setWeatherImg("https://i.postimg.cc/XYWxrv6s/sunny.png");
-        break;
-    }
-  }, [desc])
   return (
     <div className='container'>
       <div className="input">
@@ -60,11 +36,11 @@ const App = () => {
         />
         <button type="button" onClick={() => getData()}>Search</button>
       </div>
-      <div className="weather">
+      <div style={cityName.length > 0 ? { opacity: '1' } : { opacity: '0' }} className="weather">
         <h1 className="name" id="name">
           {cityName}
         </h1>
-        <img src={weatherImg} alt="" className="weather-image" />
+        <img src={`http://openweathermap.org/img/wn/${img}.png`} alt="" className="weather-image" />
         <p className="temp">
           {temp} &deg;C
         </p>
